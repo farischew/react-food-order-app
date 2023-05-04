1d63df3209a07870499774a74382be72c9412600
Faris Chew <farischew@gmail.com>
2023-05-04 17:26:28 +0800

    Adds CSS formatting to MealItem

--- src/components/Meal/MealItem.js
+++ src/components/Meal/MealItem.js
@@ -1,9 +1,11 @@
+import classes from "./MealItem.module.css";
+
 const MealItem = (props) => {
   return (
-    <li>
-      {props.meal.name}
-      {props.meal.description}
-      {props.meal.price}
+    <li className={classes.meal}>
+      <h3>{props.meal.name}</h3>
+      <p className={classes.description}>{props.meal.description}</p>
+      <p className={classes.price}>{props.meal.price}</p>
     </li>
   );
 };
