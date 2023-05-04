d7e3858522863dc67d6da67aa7e1ddae7d22d50d
Faris Chew <farischew@gmail.com>
2023-05-04 17:50:37 +0800

    Adds MealItemForm output

--- src/components/Meal/MealItem/MealItem.js
+++ src/components/Meal/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
   const price = `$${props.meal.price.toFixed(2)}`;
@@ -9,6 +10,9 @@ const MealItem = (props) => {
         <h3>{props.meal.name}</h3>
         <div className={classes.description}>{props.meal.description}</div>
         <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm />
       </div>
     </li>
   );
