18315259053ebdeb22c3b6b6b94a0ff2e7ee36d2
Faris Chew <farischew@gmail.com>
2023-05-04 17:17:09 +0800

    Adds Meals to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meal/Meals";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Meals />
     </React.Fragment>
   );
 }
