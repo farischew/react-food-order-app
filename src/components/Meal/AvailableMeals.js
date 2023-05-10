9fc20393b3d9dc2f543936ce450804469e1ad765
Faris Chew <farischew@gmail.com>
2023-05-10 12:23:36 +0800

    Adds loading message and catch http error

--- src/components/Meal/AvailableMeals.js
+++ src/components/Meal/AvailableMeals.js
@@ -5,13 +5,26 @@ import Card from "../UI/Card";
 import { useEffect, useState } from "react";
 
 const AvailableMeals = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState();
+
   const [meals, setMeals] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
+
+      const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+      await delay(1000);
+
       const response = await fetch(
         "https://react-http-90692-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json"
       );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
       const data = await response.json();
 
@@ -26,12 +39,30 @@ const AvailableMeals = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
 
-    fetchData();
+    fetchData().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
   }, []);
 
-  console.log(meals);
+  if (isLoading) {
+    return (
+      <section className={classes.mealsloading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.mealserror}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
 
   const mealsList = meals.map((meal) => (
     <MealItem
@@ -43,11 +74,10 @@ const AvailableMeals = () => {
     ></MealItem>
   ));
 
-  // console.log(mealsList);
-
   return (
     <section className={classes.meals}>
       <Card>
+        {isLoading && <p className="mealsloading">Loading...</p>}
         <ul>{mealsList}</ul>
       </Card>
     </section>
